353789661893fe0edda623c6a3ada045023bb2f4
Timer91 <marais.jeremie@gmail.com>
2019-05-10 18:53:20 +0200

    [JS] list refactor

--- src/js/list.js
+++ src/js/list.js
@@ -6,30 +6,30 @@ class List {
 			qs = n => root.querySelector( `.list${n}` );
 
 		this.rootElement = root;
-		this._title = qs( "Title" );
-		this._counter = qs( "Counter" );
-		this._cards = qs( "Cards" );
+		this._elTitle = qs( "Title" );
+		this._elCounter = qs( "Counter" );
+		this._elCards = qs( "Cards" );
 
 		this.title = title;
 		this.filters = filters;
-		this.tickets = [];
+		this.cards = [];
 
 		this._setTitle( this.title );
 	}
 
 	_setTitle( t ) {
-		this._title.textContent = t;
+		this._elTitle.textContent = t;
 	}
 	_setCounter( nb ) {
-		this._counter.innerText = `(${nb})`;
+		this._elCounter.innerText = `(${nb})`;
 	}
 
 	_fill( arr ) {
 		arr.forEach( t => {
 			const c = new Card( t.id, t.title, t.ttl );
 
-			this.tickets.push( c );
-			this._cards.append( c.rootElement );
+			this.cards.push( c );
+			this._elCards.append( c.rootElement );
 		} );
 	}
 
